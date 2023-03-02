920515b9e30605ea533dcc1644702815b814332a
Vincenzo Ruggiero <vincenzoru@live.it>
2023-03-02 17:34:16 +0100

    Add db call to display al projects' title in cards

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,27 +1,52 @@
+<template>
+  <section>
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <h1>Projects:</h1>
+        </div>
+
+        <div class="col-4">
+          <article class="card single-card" v-for="project in projects">
+            {{ project.title }}
+          </article>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
 <script>
+import axios from "axios";
+
 export default {
   name: "AppMain",
   data() {
     return {
       projects: [],
-      apiCall: "",
+      apiCall: "http://127.0.0.1:8000/api/projects/",
     };
   },
 
-  methods: {},
+  methods: {
+    getProjects() {
+      axios
+        .get(this.apiCall)
+        .then((response) => {
+          //   console.log(response.data.results);
+          this.projects = response.data.results;
+        })
+        .catch(function (error) {
+          // handle error
+          console.warn(error);
+        });
+    },
+  },
 
-  created() {},
+  created() {
+    this.getProjects();
+  },
 };
 </script>
-
-<template>
-  <section>
-    <div class="container">
-      <div class="row">
-        <h1>Vins</h1>
-      </div>
-    </div>
-  </section>
-</template>
 
 <style lang="scss" scoped></style>
