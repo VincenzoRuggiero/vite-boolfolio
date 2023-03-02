9c0ca42ad2dccc6245c5890303597db72d7c2b0b
Vincenzo Ruggiero <vincenzoru@live.it>
2023-03-02 16:58:49 +0100

    Create Main component that display all projects

---
+++ src/components/AppMain.vue
@@ -0,0 +1,27 @@
+<script>
+export default {
+  name: "AppMain",
+  data() {
+    return {
+      projects: [],
+      apiCall: "",
+    };
+  },
+
+  methods: {},
+
+  created() {},
+};
+</script>
+
+<template>
+  <section>
+    <div class="container">
+      <div class="row">
+        <h1>Vins</h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style lang="scss" scoped></style>
