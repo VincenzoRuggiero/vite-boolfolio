dc2088e6d293a22abf4ba4513d9e987ccedf1556
Vincenzo Ruggiero <vincenzoru@live.it>
2023-03-04 17:52:24 +0100

    Create single Project Card with routing button

---
+++ src/components/ProjectCard.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: "ProjectCard",
+  props: {
+    img: String,
+    title: String,
+    description: String,
+    id: Number,
+    project: Object,
+  },
+};
+</script>
+
+<template>
+  <div class="card">
+    <div class="card-body">
+      <h3 class="card-title">{{ title }}</h3>
+
+      <p class="card-text">
+        {{ description }}
+      </p>
+
+      <router-link
+        :to="{ name: 'project', params: { id: `${id}` } }"
+        class="btn btn-secondary"
+        >Open</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<style lang="scss"></style>
