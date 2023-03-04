9f878c0c586e99319759253e183ba0e1bfdfbeb5
Vincenzo Ruggiero <vincenzoru@live.it>
2023-03-04 17:52:34 +0100

    Create About page

---
+++ src/pages/AboutUs.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  name: "AboutUs",
+};
+</script>
+
+<template>
+  <div class="container">
+    <h1 class="mt-3 text-uppercase text-white">About Us</h1>
+    <div class="row">
+      <p class="text-white">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis
+        nostrum corporis a culpa in earum reiciendis consequatur numquam at?
+        Sapiente quas tempore autem aut. Quo natus veniam, quisquam aspernatur
+        temporibus blanditiis molestiae, exercitationem distinctio possimus
+        aliquam autem, soluta provident nihil voluptatibus ut at. Ducimus
+        quaerat debitis illo unde minima nisi, ut libero nemo optio blanditiis
+        recusandae consectetur adipisci asperiores cumque, facere laboriosam,
+        quidem iusto! Neque alias eum ea similique eligendi debitis repellat
+        obcaecati harum optio consequuntur quia voluptates illo ipsum amet
+        corporis, voluptate ipsa fugiat natus. Tempora illum, autem impedit
+        fugit, quod quae eius exercitationem iusto a, unde aperiam! Repellat
+        quisquam reiciendis itaque tempore maiores deserunt eligendi maxime
+        dolores quis a et quas sapiente ipsum autem voluptatem in ducimus atque,
+        aliquam provident dignissimos praesentium ex nobis. Placeat corporis
+        facere dolorum natus voluptate veniam fuga odit magni temporibus ad
+        velit labore laudantium mollitia totam esse in aut molestiae quas omnis
+        aspernatur, optio doloremque accusantium minima? Tempora dicta deserunt
+        tempore praesentium facere, eveniet omnis. Quisquam dignissimos porro
+        sunt unde tenetur pariatur, vitae neque numquam alias sequi repellat
+        minus mollitia voluptatibus, quo ut quaerat blanditiis possimus quas
+        similique cupiditate odit. Doloribus odit, facilis consequuntur vel quod
+        alias, doloremque non libero excepturi, quidem fuga.
+      </p>
+    </div>
+  </div>
+</template>
+
+<style lang=""></style>
