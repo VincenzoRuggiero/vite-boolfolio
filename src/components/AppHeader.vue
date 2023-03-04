2b13f7c631e4d17666226b07886b080e9d1fdc9e
Vincenzo Ruggiero <vincenzoru@live.it>
2023-03-04 17:51:45 +0100

    Create header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,38 @@
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">Boolfolio</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNavAltMarkup"
+        aria-controls="navbarNavAltMarkup"
+        aria-expanded="false"
+        aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div class="navbar-nav">
+          <router-link class="nav-link" :to="{ name: 'home' }"
+            >Home</router-link
+          >
+          <router-link class="nav-link" :to="{ name: 'projects' }"
+            >Projects</router-link
+          >
+          <router-link class="nav-link" :to="{ name: 'about-us' }"
+            >About Us</router-link
+          >
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style></style>
