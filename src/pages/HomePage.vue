d218b660e70555167ea21d196a481363721c6053
Vincenzo Ruggiero <vincenzoru@live.it>
2023-03-04 17:52:52 +0100

    Create Homepage component

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,24 @@
+<script>
+import AppHeader from "../components/AppHeader.vue";
+
+export default {
+  name: "HomePage",
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<template>
+  <main>
+    <div class="container">
+      <div class="row">
+        <div class="col-12 d-flex justify-content-center py-5 text-white">
+          <h1>Welcome to Boolfolio</h1>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style></style>
